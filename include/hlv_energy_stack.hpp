d70794e996b3a9b019cf369fc2fcfe49d0488fc2
Don Michael Feeney Jr <dfeen87@gmail.com>
2025-12-13 09:39:39 -0500

    Add hlv_energy_stack.hpp header file

---
+++ include/hlv_energy_stack.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "hlv_bms_middleware_v2.hpp"
+#include "hlv_torque_enhancement_v2.hpp"
+#include "hlv_regen_braking_manager_v1.hpp"
+#include "hlv_energy_recovery_coordinator_v1.hpp"
